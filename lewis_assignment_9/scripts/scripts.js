55b2dbb0d2203a43e96dcf7776dcf57a93409e1d
Richard Lewis <97256133+Tirialoxliem@users.noreply.github.com>
2023-06-06 17:38:30 -0400

    Add files via upload

---
+++ lewis_assignment_9/scripts/scripts.js
@@ -0,0 +1,143 @@
+function myFunction2a() {
+  alert("You chose berries!");
+}
+
+function myFunction2b() {
+  alert("You chose deserts!");
+}
+
+function myFunction2c() {
+  alert("You chose eggs!");
+}
+
+function myFunction2d() {
+  alert("You chose fish!");
+}
+
+function myFunction2e() {
+  alert("You chose hamburgers!");
+}
+
+function myFunction2f() {
+  alert("You chose meat!");
+}
+
+function myFunction2g() {
+  alert("You chose nuts!");
+}
+
+function myFunction2h() {
+  alert("You chose pasta!");
+}
+
+function myFunction2i() {
+  alert("You chose pizza!");
+}
+
+function myFunction2j() {
+  alert("You chose seafood!");
+}
+
+function myFunction2k() {
+  alert("You chose snacks!");
+}
+
+function myFunction2l() {
+  alert("You chose vegetables!");
+}
+
+function myFunction3a() {
+  alert("You chose black!");
+}
+
+function myFunction3b() {
+  alert("You chose blue!");
+}
+
+function myFunction3c() {
+  alert("You chose brown!");
+}
+
+function myFunction3d() {
+  alert("You chose gray!");
+}
+
+function myFunction3e() {
+  alert("You chose green!");
+}
+
+function myFunction3f() {
+  alert("You chose orange!");
+}
+
+function myFunction3g() {
+  alert("You chose pink!");
+}
+
+function myFunction3h() {
+  alert("You chose purple!");
+}
+
+function myFunction3i() {
+  alert("You chose red!");
+}
+
+function myFunction3j() {
+  alert("You chose teal!");
+}
+
+function myFunction3k() {
+  alert("You chose white!");
+}
+
+function myFunction3l() {
+  alert("You chose yellow! That is my favorite color also!!!");
+}
+
+function myFunction4a() {
+  alert("You like doing art!");
+}
+
+function myFunction4b() {
+  alert("You like to bowl!");
+}
+
+function myFunction4c() {
+  alert("You like to camp!");
+}
+
+function myFunction4d() {
+  alert("You like to restore cars!");
+}
+
+function myFunction4e() {
+  alert("You like to garden!");
+}
+
+function myFunction4f() {
+  alert("You like to play golf!");
+}
+
+function myFunction4g() {
+  alert("You like to hike!");
+}
+
+function myFunction4h() {
+  alert("You like to jog!");
+}
+
+function myFunction4i() {
+  alert("You like to read! You must be a smart cookie!");
+}
+
+function myFunction4j() {
+  alert("You like to play sports!");
+}
+
+function myFunction4k() {
+  alert("You like to swim!");
+}
+
+function myFunction4l() {
+  alert("You like something different!");
+}
